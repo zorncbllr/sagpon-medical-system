eeda985f738899f955616b8e71bb03ad47fae9a4
Manuel <caballeromanuel203@gmail.com>
2024-11-17 07:52:39 -0800

    changed the logo

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "../components/login-form";
-import { FramerLogoIcon } from "@radix-ui/react-icons";
+import Logo from "../assets/logo";
 
 function Login() {
   return (
@@ -10,7 +10,7 @@ function Login() {
 
       <section className="w-1/2 h-full bg-white flex flex-col gap-6 items-center justify-center px-8">
         <div className="grid place-items-center gap-4">
-          <FramerLogoIcon color="green" />
+          <Logo />
           <h1 className="text-4xl font-semibold">Sagpon Health Station</h1>
           <p className="text-center text-slate-500 before:content-[''] before:h-[0.1px] before:bg-slate-300 before:block before:m-4 after:content-[''] after:h-[0.1px] after:bg-slate-300 after:block after:m-4">
             Providing compassionate care and innovative solutions for a
