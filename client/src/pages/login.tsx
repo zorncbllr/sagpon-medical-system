5daaf39c3ec61eeb376fb1d76d81d5cfa90c82c8
Manuel <caballeromanuel203@gmail.com>
2024-11-17 19:13:00 -0800

    minor animation adjustments

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -13,13 +13,13 @@ function Login() {
 
       <section className="w-[55rem] h-full bg-white flex flex-col gap-6 items-center justify-center px-12 shadow-xl">
         <div className="grid place-items-center gap-4">
-          <BoxReveal duration={0.2}>
+          <BoxReveal duration={0.4}>
             <Logo />
           </BoxReveal>
-          <BoxReveal duration={0.2}>
+          <BoxReveal duration={0.6}>
             <h1 className="text-4xl font-semibold">Sagpon Health Station</h1>
           </BoxReveal>
-          <BoxReveal duration={0.2}>
+          <BoxReveal duration={0.5}>
             <p className="text-center text-slate-500 before:content-[''] before:h-[0.1px] before:bg-slate-300 before:block before:m-4 after:content-[''] after:h-[0.1px] after:bg-slate-300 after:block after:m-4">
               Providing compassionate care and innovative solutions for a
               healthier community.
