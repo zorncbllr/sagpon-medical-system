a7ae8b8354da6c5371eff56860b341c2d11d722d
Manuel <caballeromanuel203@gmail.com>
2024-11-26 20:49:19 -0800

    working on authentication backend

--- server/src/controllers/Home.php
+++ server/src/controllers/Home.php
@@ -9,18 +9,3 @@ class Home extends Controller
 		return json(['msg' => 'Initial set-up done.']);
 	}
 }
-#[Route(method: 'GET', path: '/home')]
-public function landingPage(Request $request)
-{
-	return view('home', [
-		'title' => 'Clinic Management System',
-		'welcomeMessage' => 'Welcome to the Clinic Management System',
-		'features' => [
-			'Patient Management',
-			'Appointment Scheduling',
-			'Billing and Invoicing',
-			'Medical Records',
-			'Reporting and Analytics'
-		]
-	]);
-}
